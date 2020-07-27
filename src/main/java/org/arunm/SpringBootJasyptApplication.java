3b91492aa3ffe58eb91296d36cb9b846708d5de6
Arun Menon <arun.menon@autogeneral.com.au>
2020-07-27 21:13:09 +1000

    Without encrypted properties

---
+++ src/main/java/org/arunm/SpringBootJasyptApplication.java
@@ -0,0 +1,32 @@
+package org.arunm;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@EnableConfigurationProperties(SpringBootJasyptApplication.TopSecret.class)
+public class SpringBootJasyptApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext configApp =  SpringApplication.run(SpringBootJasyptApplication.class,args);
+        System.out.println(configApp.getBean(TopSecret.class).getPassword());
+    }
+
+    @ConfigurationProperties(prefix="app")
+    public static class TopSecret {
+
+        private String password;
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+}
